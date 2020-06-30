c87351e2eedc306a940d885d2e295b6852abd142
MUTHAKABRIAN <muthakabrian@gmail.com>
2020-06-30 23:14:00 +0300

    add branching if else

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,7 +4,22 @@ import java.util.Scanner;
 public class App {
     public static void main (String[]args){
         Console myConsole = System.console();
-        Scanner scanner = new Scanner(System.in);
+        Scanner myScanner = new Scanner(System.in);
         boolean runProgram = true;
+
+        while(runProgram){
+            System.out.println("welcome to Caesar Cipher");
+            System.out.println ("Would you like encrypt or decrypt a statement");
+            String userAnswer= myConsole.readLine();
+
+            if (userAnswer.equals("encrypt")){
+                System.out.println("Please enter a statement that you would like to encrypt: ");
+                String statement = myConsole.readLine();
+
+                System.out.println("Please enter an encryption key:");
+                int key= myScanner.nextInt();
+
+            }
+        }
     }
 }
