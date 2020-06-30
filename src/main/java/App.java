e75d6a1977931cba9fd2b9eee05e4719f5a65dc2
MUTHAKABRIAN <muthakabrian@gmail.com>
2020-06-30 18:55:47 +0300

    add main function

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,4 +2,9 @@ import java.io.Console;
 import java.util.Scanner;
 
 public class App {
+    public static void main (String[]args){
+        Console myConsole = System.console();
+        Scanner scanner = new Scanner(System.in);
+        boolean runProgram = true;
+    }
 }
