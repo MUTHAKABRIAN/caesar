b6e140033466515e36b755e8cf9119e1866fa0da
MUTHAKABRIAN <muthakabrian@gmail.com>
2020-06-30 18:02:59 +0300

    create CaesarCipher.java to hold buisness logic

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
-
 public class CaesarCipher {
+    private String mStatement;
+    private int mKey;
+
+    public CaesarCipher(String statement,int key) {
+        this.mStatement = statement;
+        this.mKey = key;
+    }
+    public String getStatement(){
+        return mStatement;
+    }
+    public int getKey(){
+        return mKey;
+    }
+    public String isActuallyEncrypted(String statement, int key){
+        String result="";
+        StringBuffer encrypted = new StringBuffer();
+
+        for (int i = 0; i < statement.length();i++){
+            int encrypt =statement.charAt(i);
+
+            if (encrypt +key>122){
+                char encrypts = (char) (encrypt-26+key);
+                encrypted.append(encrypt);
+            }
+            else{
+                char encrypts =(char ) (encrypt+(key % 26));
+                encrypted.append(encrypts);
+            }
+
+            }
+        result += encrypted;
+        return result;
+        }
+    }
+
 }
